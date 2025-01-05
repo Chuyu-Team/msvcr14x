a066dfeb1e34af94eca125c25d59b323c9e18f29
mingkuang <mingkuang@live.com>
2025-01-05 11:37:28 +0800

    Opt，ntdll、boost-math改用模块引用、YY-Thunks改用nuget引用

--- mfc14x/mfc14x.cpp
+++ mfc14x/mfc14x.cpp
@@ -4,20 +4,3 @@
 #include "stdafx.h"
 
 #include "..\msvcr14x\stdafx.cpp"
-
-#ifdef _M_IX86
-#pragma comment(lib,"KERNEL32.lib")
-#pragma comment(lib,"win2k_kernl32p.lib")
-#elif defined(_M_X64)
-#pragma comment(lib,"win2k3_KERNEL32.lib")
-#pragma comment(lib,"win2k3_kernl32p.lib")
-#endif
-
-#pragma comment(linker, "/ENTRY:DllMainCRTStartupForYY_Thunks")
-extern "C"
-{
-	const void* __pfnDllMainCRTStartupForYY_Thunks;
-	BOOL __YY_Thunks_Disable_Rreload_Dlls;
-	const void* __pfnYY_Thunks_CustomLoadLibrary;
-	const void* __acrt_atexit_table;
-}
