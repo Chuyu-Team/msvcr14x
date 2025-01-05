93f36387b474d96a244f1e6bb0cabf07d31ab772
mingkuang <mingkuang@live.com>
2025-01-05 11:37:28 +0800

    Opt，ntdll、boost-math改用模块引用、YY-Thunks改用nuget引用

--- msvcp14x/msvcp14x.cpp
+++ msvcp14x/msvcp14x.cpp
@@ -3,15 +3,6 @@
 
 #include "stdafx.h"
 
-
-#ifdef _M_IX86
-#pragma comment(lib,"KERNEL32.lib")
-#pragma comment(lib,"win2k_kernl32p.lib")
-#elif defined(_M_X64)
-#pragma comment(lib,"win2k3_KERNEL32.lib")
-#pragma comment(lib,"win2k3_kernl32p.lib")
-#endif
-
 //由于无法修改winapisupp.cpp,所以目前通过下面这样来防止pinit的符号因我们的链接选项中指定了/OPT:REF而被剔除
 #include "winapisupp.cpp"
 #ifndef _WIN64
@@ -20,12 +11,3 @@
 #pragma comment(linker, "/include:?force_included__winapisupp_pinit@@3P6AHXZEA")
 #endif
 _CRTALLOC(".CRT$XIC") /*static*/ _PIFV force_included__winapisupp_pinit = initialize_pointers;
-
-#pragma comment(linker, "/ENTRY:DllMainCRTStartupForYY_Thunks")
-extern "C"
-{
-	const void* __pfnDllMainCRTStartupForYY_Thunks;
-	BOOL __YY_Thunks_Disable_Rreload_Dlls;
-	const void* __pfnYY_Thunks_CustomLoadLibrary;
-	const void* __acrt_atexit_table;
-}
--- msvcr14x/msvcr14x.cpp
+++ msvcr14x/msvcr14x.cpp
@@ -4,19 +4,7 @@
 #include "stdafx.h"
 
 
-#ifdef _M_IX86
-#pragma comment(lib,"KERNEL32.lib")
-#pragma comment(lib,"win2k_kernl32p.lib")
-#elif defined(_M_X64)
-#pragma comment(lib,"win2k3_KERNEL32.lib")
-#pragma comment(lib,"win2k3_kernl32p.lib")
-#endif
 
-#pragma comment(linker, "/ENTRY:DllMainCRTStartupForYY_Thunks")
-extern "C"
-{
-	BOOL WINAPI __acrt_DllMain(_In_ HINSTANCE hInstance, _In_ DWORD fdwReason, _In_ LPVOID lpvReserved);
-	const void* __pfnDllMainCRTStartupForYY_Thunks = __acrt_DllMain;
-	BOOL __YY_Thunks_Disable_Rreload_Dlls;
-	const void* __pfnYY_Thunks_CustomLoadLibrary;
-}
+extern "C" BOOL WINAPI __acrt_DllMain(_In_ HINSTANCE hInstance, _In_ DWORD fdwReason, _In_ LPVOID lpvReserved);
+
+extern "C" const void* __pfnDllMainCRTStartupForYY_Thunks = __acrt_DllMain;
